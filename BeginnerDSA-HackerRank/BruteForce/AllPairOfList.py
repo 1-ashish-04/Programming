7603f9f7f4930f7dacaa575adba336460d935dbb
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-03-28 17:38:47 +0530

    Pair Problem

--- BeginnerDSA-HackerRank/BruteForce/AllPairOfList.py
+++ BeginnerDSA-HackerRank/BruteForce/AllPairOfList.py
@@ -1,13 +1,17 @@
 # Brute Force
 ar = [1,2,3,4,5]
 
-# Approach one consider all the pairs  (With repeatation)
+# Approach one 
+
+# consider all the pairs  (With repeatation)
 for i in ar:
     for j in ar:
         print( i,j, end=" | ")
     print("")
 
-# Approach two when i<j (Without repeatation) 
+# Approach two 
+
+# when i<j (Without repeatation) 
 # i start from 0  and   j start from j = i + 1
 print("i < j")
 for i in range(len(ar)):
@@ -16,14 +20,14 @@ for i in range(len(ar)):
     print("")
 
 
-# Print all the sum of pairs
+# 1. Print all the sum of pairs
 
 for i in ar:
     for j in ar:
         print(i,'+',j,'-->', i+j, end=" | ")
     print()
 
-# print pairs sums with i<j , two different numbers
+# 2. print pairs sums with i<j , two different numbers
 print(" ")
 for i in range(len(ar)):
     for j in range(i+1,len(ar)):
@@ -31,7 +35,7 @@ for i in range(len(ar)):
     print()
 
 
-#  print all the pairs (i,j) where sum=k and i<j
+# 3. print all the pairs (i,j) where sum=k and i<j
 # Two sum
 sum = 7
 l = []
@@ -47,8 +51,8 @@ for i in range(len(ar)):
 print(l)
 
 
-#  Triplet sum
-# u need to find a[i]+a[j]+a[k]==sum
+#  4. Triplet sum
+# u need to find a[i]+a[j]+a[k] == sum
 # and i<j<k
 
 
@@ -57,3 +61,14 @@ for i in range(len(ar)):
         for k in range(j+1,len(ar)):
             print(ar[i]+ar[j]+ar[k],end=" | ")
         print("")
+
+# 5. we need to find pairs
+# such that a[j]-a[i]==k 
+# i<j
+
+k = 2
+for i in range(len(ar)):
+    for j in range(i+1, len(ar)):
+        if(ar[j] - ar[i] == k): # works only when there is a sorted list, for un sorted list need to use a abs() method.
+            print(ar[j], ar[i], end=" | ")
+    print()
