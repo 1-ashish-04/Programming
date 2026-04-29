ff12bd61ebdee5395579bdc4bc72aec914ffd459
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-04-29 20:29:49 +0530

    Best Time To Buy and Sell Stock

---
+++ Module 3 hashmap_dict/lookUpDataStructure/bestTimetoBuyandSellStock.py
@@ -0,0 +1,21 @@
+# Best Time To Buy And Sell Stock
+
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+def maxProfit(prices):
+    minBuy = float('inf')  # track lowest price to buy stock
+    maxProfit = 0 # calculate max Profit to sell the stock  (Sell - Buy of stack maximum)
+    for i in prices:
+        if i < minBuy:
+            minBuy = i
+        elif (i - minBuy) > maxProfit:
+            maxProfit = i - minBuy
+    return maxProfit
+
+print(maxProfit([7,1,5,3,6,4]))
+
+print(maxProfit([7,6,4,3,1]))
