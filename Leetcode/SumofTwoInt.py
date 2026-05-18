14016c416477eb668ffd7ffd2092fa073e2be356
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-05-18 22:17:00 +0530

    Sum Of Two Integer

---
+++ Leetcode/SumofTwoInt.py
@@ -0,0 +1,12 @@
+# Sum Of Two Integer
+
+# Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+class Solution:
+
+    def sumOfTwo(self, a, b):
+        return sum([a,b])
+
+s = Solution()
+
+print(s.sumOfTwo(4,5))
