6c3fe7e5a7898c720d6b9ac9cb58663c86964093
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-05-19 19:10:27 +0530

    Arithmetic Operation Python

---
+++ Module 7 Maths/ArtihmeticOp.py
@@ -0,0 +1,7 @@
+print(4 + 2)
+print(4 - 2)
+print(4 * 2)
+print(4 / 2) # resul with decimal
+print(4 % 2)
+print(4 // 2) # integer value in result
+print(4 ** 2) # used to calculate the power of an number
