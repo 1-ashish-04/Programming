4b68e869f9f0411e7943d448c495d73b4e1b0493
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-05-22 10:19:38 +0530

    Vowels Of String

---
+++ Module 4 Two-Pointers/RevVowelOfStr.py
@@ -0,0 +1,42 @@
+# Reverse Vowels of a String
+
+# Given a string s, reverse only all the vowels in the string and return it.
+
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+# Example 1:
+
+# Input: s = "IceCreAm"
+
+# Output: "AceCreIm"
+
+# Explanation:
+
+# The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+# Example 2:
+
+# Input: s = "leetcode"
+
+# Output: "leotcede"
+
+class Solution:
+    def reverseVowels(self, s):
+        s = list(s)
+        i = 0
+        j = len(s)-1
+        vowels = set('aeiouAEIOU')
+        while(i<j):
+            if s[i] not in vowels:
+                i += 1
+            elif s[j] not in vowels:
+                j -= 1
+            else:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+        return "".join(s)
+    
+s = Solution()
+
+print(s.reverseVowels("Icecream"))
